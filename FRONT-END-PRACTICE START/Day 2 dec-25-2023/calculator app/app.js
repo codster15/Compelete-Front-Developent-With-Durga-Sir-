60f0848e014596ab3cedbdf050a8c5c713f2dbc4
Codster dev <deepaksingh.desire@gmail.com>
2023-12-28 23:13:00 +0530

    upload

--- FRONT-END-PRACTICE START/Day 2 dec-25-2023/calculator app/app.js
+++ FRONT-END-PRACTICE START/Day 2 dec-25-2023/calculator app/app.js
@@ -1,57 +1,76 @@
 
-var dataStore = document.querySelector("#data")
-var value_1 = document.querySelector("#one")
-var value_add = document.querySelector("#add")
-var value_clear = document.querySelector("#clear")
-
-const Zero = 0;
-const one = "1";
-const two = 2;
-const three = 3;
-const four = 4;
-const five = 5;
-const six = 6;
-const seven = 7;
-const eight = 8;
-const nine = 9;
-const doubleZero = "00";
 
 
-var no1 = one
 
-    var number1 = ()=>{
-            
-        if( no1 == "1111111"){
-            alert("Number Exceeded");
-            return
-            
-          }
-        console.log(typeof(no1) );
-      dataStore.innerHTML = no1;
-      console.log( no1);
-      no1 = no1 + one;
+// explaination the code
 
-      
+      // first we pass use the function  display () in html element along with same parameter
+      // then we catch the parameter  in javscript file and stored the ctached paramenter input display 
 
+      // then we made the second fumction to calculte the stored result with the help of (eval) method and show back to the user on screen
+
+
+
+
+
+
+
+
+
+let input = document.querySelector("#data")
+
+
+
+
+
+let initialDisplay = true;
+
+display = (numberPass) => {
+
+  if(input.innerHTML.length == 12){
+    alert("Number got exceeded !")
+    return
+}
+// Inteligent code 
+    // Check if it's the initial display
+    if (initialDisplay) {
+        input.innerHTML = numberPass; // Replace "0" with the new input
+        initialDisplay = false; // Set initial display to false
+    } else {
+        input.innerHTML += numberPass; // Otherwise, append the new input
     }
 
 
-value_1.addEventListener("click" , ()=>{
-    number1();
-    })
+    console.log(input.innerHTML.length);
+
+}
+
+function calculate (){
+
+    if(input.innerHTML == "" ){
+
+      alert("enter any number")
+      return
+    }
+  var result = input.innerHTML;
+  result = eval(result);       
+  
+  // evalmethod is responsible for doing the mathematical operation we gave the number --> eval     perfore the operation
+  
+
+  input.innerHTML = result;
+
+}
+
+ function AllClear (){   
+  input.innerHTML = ""     //here we make the result 0 means not to show any thing on display
+  
+ }
 
 
-value_add.addEventListener("click" , ()=>{
-        const one = "+"; 
-        console.log(one);
-        document.querySelector("#data").innerHTML = one;
-        console.log(dataStore);
-        no1 = 1
-    })
 
-    value_clear.addEventListener("click" , ()=>{
-    
-        document.querySelector("#data").innerHTML = "";
-        console.log("a")
-        no1 = 1
-    })
+ function DeleteLstNumber (){
+
+ input.innerHTML =  input.innerHTML.slice(0,-1)
+  
+ }
