d452d0ab0839d25494d736ebc5514ea4f50d5c2b
Codster dev <deepaksingh.desire@gmail.com>
2024-01-02 00:14:50 +0530

    upload

---
+++ java code/test.java
@@ -0,0 +1,12 @@
+import javax.print.DocFlavor.STRING;
+
+/**
+  * test
+  */
+ public class test {
+ 
+    public static void main(String[] args) {
+        String a = "welcome"; 
+        System.out.println(a);
+    }
+ }
